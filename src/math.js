9c3e4679d274e21e1e1d71be261f35648c3300f1
Gen Ichino <zenoplex@gmail.com>
2017-11-18 17:16:06 +0900

    feat: add basic math operation

---
+++ src/math.js
@@ -0,0 +1,27 @@
+// @flow
+type Constrain = (number, number) => number => number;
+export const constrain: Constrain = (min, max) => n =>
+  Math.max(Math.min(n, max), min);
+
+type Map = (number, number, number, number, _?: boolean) => number => number;
+export const map: Map = (
+  minInput,
+  maxInput,
+  minOutput,
+  maxOutput,
+  fit,
+) => (n) => {
+  // prettier-ignore
+  const v = (((n - minInput) / (maxInput - minInput)) * (maxOutput - minOutput)) + minOutput;
+
+  if (!fit) return v;
+  return minOutput < maxOutput
+    ? constrain(minOutput, maxOutput)(v)
+    : constrain(maxOutput, minOutput)(v);
+};
+
+type ToRadians = number => number;
+export const toRadians: ToRadians = angle => angle * (180 / Math.PI);
+
+type ToDegrees = number => number;
+export const toDegree: ToDegrees = angle => angle * (Math.PI / 180);
